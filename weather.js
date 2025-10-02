d3af2e841bdafad9c72ef8544444ec357f4961e9
eirlysia@proton.me <eirlysia@proton.me>
2025-10-02 12:22:53 +0300

    Changed api call

--- weather.js
+++ weather.js
@@ -11,7 +11,7 @@ export class Weather {
         console.log(this.location.latitude);
         console.log(this.location.longitude);
 
-        const url = `https://api.open-meteo.com/v1/forecast?latitude=${this.location.latitude}&longitude=${this.location.longitude}&timezone=auto&current=temperature_2m,weather_code`;
+        const url = `https://api.open-meteo.com/v1/forecast?latitude=${this.location.latitude}&longitude=${this.location.longitude}&timezone=auto&current=temperature_2m,is_day,cloud_cover,weather_code,precipitation,rain,showers,snowfall`;
         try {
             const response = await fetch(url);
             if(!response.ok) {
@@ -20,6 +20,11 @@ export class Weather {
             const result = await response.json();
             this.temperature2m = result.current.temperature_2m;
             this.weatherCode = result.current.weather_code;
+            this.time = result.current.time;
+            this.timezone = result.timezone;
+            this.is_day = result.current.is_day;
+            this.cloud_cover = result.current.cloud_cover;
+
             // weather = result;
             // console.log(weather);
             console.log(this);
